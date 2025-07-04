01780f620fc3a461a86874b7df20537d14a7bfbb
oOccasio <dhzktldh@gmail.com>
2025-07-04 20:39:36 +0900

    [feat] #1 스프링 초기 세팅

---
+++ src/test/java/dgu/umc_app/MiruniApplicationTests.java
@@ -0,0 +1,13 @@
+package dgu.umc_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiruniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
