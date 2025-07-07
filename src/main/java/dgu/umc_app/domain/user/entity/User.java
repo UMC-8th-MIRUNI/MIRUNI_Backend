fe917fa7308a99dda1b9f1454a35a5bc4f1ed8b5
jjin70 <jjin0434@naver.com>
2025-07-07 13:52:26 +0900

    [feat] #6 User Entity 생성

---
+++ src/main/java/dgu/umc_app/domain/user/entity/User.java
@@ -0,0 +1,55 @@
+package dgu.umc_app.domain.user.entity;
+
+import dgu.umc_app.global.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 20)
+    private String name;
+
+    @Column(nullable = false, length = 50)
+    private String email;
+
+    @Column(nullable = false, length = 255)
+    private String phoneNumber;
+
+    @Column(nullable = false, length = 20)
+    private String nickname;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private boolean passwordExpired;
+
+    private LocalDateTime lastPasswordChanged;
+
+    @Column(nullable = false)
+    private boolean agreedPrivacyPolicy;
+
+    @Column(nullable = false)
+    private int peanutCount = 0;
+
+    private Integer popupAlarmInterval;
+
+    private Integer bannerAlarmInterval;
+
+    @Column(nullable = false, length = 50)
+    private String userPreference;
+
+    @Enumerated(EnumType.STRING)
+    private OauthProvider oauthProvider;
+}
