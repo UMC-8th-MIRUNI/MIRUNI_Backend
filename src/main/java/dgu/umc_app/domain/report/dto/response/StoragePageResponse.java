51b91ffd1a26185a35c60bcd7cafb82d4d607f93
Suhyeon7 <zxguszx@naver.com>
2025-08-06 23:15:46 +0900

    [refactor] #77 StoragePageResponse 스키마 추가

--- src/main/java/dgu/umc_app/domain/report/dto/response/StoragePageResponse.java
+++ src/main/java/dgu/umc_app/domain/report/dto/response/StoragePageResponse.java
@@ -1,16 +1,30 @@
 package dgu.umc_app.domain.report.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 @Builder
 public record StoragePageResponse(
-        int peanutCount,    //사용자 땅콩 수
-        int completionRatePercent,  //이번달 일정 달성률
-        boolean isOpenedThisMonth,  //이번달 리포트 오픈 여부
-        boolean canOpenThisMonth,   //이번달 리포트 오픈조건(땅콩 ≥ 30 && 완료율 ≥ 80%) 충족 여부
-        boolean isOpenedLastMonth,  //저번달 리포트 오픈 여부
-        String lockState,            // "잠김" or "열림"
-        boolean isOpenButtonVisible  // true면 '오픈하기' 버튼 보여줌
+        @Schema(description = "사용자 땅콩 수")
+        int peanutCount,
+
+        @Schema(description = "이번달 일정 달성률")
+        int completionRatePercent,
+
+        @Schema(description = "이번달 리포트 오픈 여부")
+        boolean isOpenedThisMonth,
+
+        @Schema(description = "이번달 리포트 오픈조건(땅콩 ≥ 30 && 완료율 ≥ 80%) 충족 여부")
+        boolean canOpenThisMonth,
+
+        @Schema(description = "저번달 리포트 오픈 여부")
+        boolean isOpenedLastMonth,
+
+        @Schema(description = "\"잠김\" or \"열림\"")
+        String lockState,
+
+        @Schema(description = "true면 '오픈하기' 버튼 보여주기 위함")
+        boolean isOpenButtonVisible
 ) {
     public static StoragePageResponse from(
             int peanutCount,
